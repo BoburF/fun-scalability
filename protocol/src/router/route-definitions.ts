484f5ff733d7b279ca4e4293ef69c3b7d60044e0
BoburF <changeitgo3@gmail.com>
2025-08-16 10:52:05 +0500

    fix: some changes from removal

--- protocol/src/router/route-definitions.ts
+++ protocol/src/router/route-definitions.ts
@@ -1,9 +1,9 @@
-import { InferSchemaType, SchemaValidator } from "../format/schema-validator";
+import { SchemaValidator } from "../format/schema-validator";
 
 export type RouteDefinition<V extends SchemaValidator<any>> = {
     apiKey: number;
     validator: V;
-    handler(params: InferSchemaType<V>): Promise<unknown> | unknown;
+    handler(params: V): Promise<unknown> | unknown;
 };
 
 export type RouteDefinitions<T extends SchemaValidator<any>[]> =
