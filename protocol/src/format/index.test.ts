8361dc395c9c09c2ef47d572bdb719594ecc3f09
BoburF <changeitgo3@gmail.com>
2025-08-18 15:29:17 +0500

    fix: array also tested

--- protocol/src/format/index.test.ts
+++ protocol/src/format/index.test.ts
@@ -8,6 +8,11 @@ import {
 const getInfoSchema = SchemBuilder.schemaDefinition({
     apiKey: SchemBuilder.number16(),
     src: SchemBuilder.string(),
+    arr: SchemBuilder.array(
+        SchemBuilder.schemaDefinition({
+            num: SchemBuilder.number8(),
+        }),
+    ),
 });
 
 type GetInfoParams = InferSchema<typeof getInfoSchema>;
@@ -15,6 +20,7 @@ type GetInfoParams = InferSchema<typeof getInfoSchema>;
 const data: GetInfoParams = {
     apiKey: 2,
     src: "bobur zo'r bola",
+    arr: [{ num: 12 }, { num: 63 }],
 };
 
 const buffer = buildData(data, getInfoSchema);
