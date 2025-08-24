05b791fc6db51f72a4d1e7a78ba2619835c69b48
BoburF <changeitgo3@gmail.com>
2025-08-24 13:06:00 +0500

    fix: error handling?

--- back/src/infra/request-controller/request-controller.ts
+++ back/src/infra/request-controller/request-controller.ts
@@ -1,4 +1,5 @@
 import { buildData, parseData, SchemaBuilder, SchemaDefinition } from "protocol";
+import { CustomError } from "../../domain/_core/error";
 
 export interface RequestController {
     handler(data: Buffer): Promise<Buffer>;
@@ -20,22 +21,35 @@ export class RequestControllerImpl implements RequestController {
     constructor(private handlers: RequestHandlers) {}
 
     async handler(data: Buffer): Promise<Buffer> {
-        const headDataSchema = SchemaBuilder.schemaDefinition({
-            apiKey: SchemaBuilder.number16(),
-        });
+        try {
+            const headDataSchema = SchemaBuilder.schemaDefinition({
+                apiKey: SchemaBuilder.number16(),
+            });
 
-        const header = parseData(data, headDataSchema);
+            const header = parseData(data, headDataSchema);
 
-        const handler = this.handlers[header.apiKey];
+            const handler = this.handlers[header.apiKey];
 
-        if (!handler) {
-            throw new Error(`Handler for apiKey=${header.apiKey} is not found`);
+            if (!handler) {
+                throw new Error(`Handler for apiKey=${header.apiKey} is not found`);
+            }
+
+            const params = parseData(data.subarray(this.headerSize), handler.paramSchema);
+
+            const result = await handler.handle(params);
+
+            return buildData(result, handler.resultSchema);
+        } catch (err) {
+            const errorSchema = SchemaBuilder.schemaDefinition({
+                errorCode: SchemaBuilder.number32(),
+                data: SchemaBuilder.string(),
+            });
+
+            if (err instanceof CustomError) {
+                return buildData(err.toJSON(), errorSchema);
+            }
+
+            return buildData({ errorCode: -100, data: `${err}` }, errorSchema);
         }
-
-        const params = parseData(data.subarray(this.headerSize), handler.paramSchema);
-
-        const result = await handler.handle(params);
-
-        return buildData(result, handler.resultSchema);
     }
 }
